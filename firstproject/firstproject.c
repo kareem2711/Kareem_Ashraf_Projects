85be45bb18a5682e9f90e1ef43eb345de9b68d2f
Kareem Ashraf <143561470+kareem2711@users.noreply.github.com>
2023-12-12 18:14:11 +0200

    Add files via upload

---
+++ firstproject/firstproject.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : firstproject.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : printing c programming, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+	printf("c programming ");
+	fflush(stdin); fflush(stdout);
+
+
+}
+
---
+++ firstproject/src/firstproject.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : firstproject.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+	printf("c programming ");
+	fflush(stdin); fflush(stdout);
+
+
+}
+
